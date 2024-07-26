0322aef882d52f842f0b0e9225640081c83f51f5
Anna Taranchuk <taranchuk4@yahoo.com>
2024-07-26 21:50:52 +0300

    add crud

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -1,4 +1,10 @@
-import { getAllStudents, getStudentById } from "../services/students.js";
+import {
+  getAllStudents,
+  getStudentById,
+  createStudent,
+  deleteStudent,
+  updateStudent,
+} from "../services/students.js";
 
 // 1. Імпортуємо функцію з бібліотеки
 import createHttpError from "http-errors";
@@ -46,3 +52,76 @@ export const getStudentByIdController = async (req, res, next) => {
 
 //Після виклику next, обробник помилок в нашому додатку (error middleware)
 //у файлі server.js, перехопить і опрацює цю помилку.
+
+//Використаємо функцію сервісу у контролері. Для запитів, які щось створюють,
+//семантично правильно відправляти відповідь зі статус - кодом 201 Created.:
+
+export const createStudentController = async (req, res) => {
+  const student = await createStudent(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: `Successfully created a student!`,
+    data: student,
+  });
+};
+
+//Використаємо функцію сервісу у контролері та обробку помилки,
+//якщо студента по такому id не буде знайдно.У разі успішного
+//видалення повернемо порожню відповідь із статусом 204 No Content:
+
+export const deleteStudentController = async (req, res, next) => {
+  const { studentId } = req.params;
+
+  const student = await deleteStudent(studentId);
+
+  if (!student) {
+    next(createHttpError(404, "Student not found"));
+    return;
+  }
+
+  res.status(204).send();
+};
+
+//Метод PUT, згідно зі специфікацією, призначений для оновлення існуючого ресурсу
+//за вказаним ID або створення нового, якщо такий ресурс відсутній.
+//Для того, щоб функція updateStudent могла не тільки оновлювати,
+//але й створювати ресурс при його відсутності, необхідно їй аргументом
+//додатково передати { upsert: true }.
+
+export const upsertStudentController = async (req, res, next) => {
+  const { studentId } = req.params;
+
+  const result = await updateStudent(studentId, req.body, {
+    upsert: true,
+  });
+
+  if (!result) {
+    next(createHttpError(404, "Student not found"));
+    return;
+  }
+
+  const status = result.isNew ? 201 : 200;
+
+  res.status(status).json({
+    status,
+    message: `Successfully upserted a student!`,
+    data: result.student,
+  });
+};
+
+export const patchStudentController = async (req, res, next) => {
+  const { studentId } = req.params;
+  const result = await updateStudent(studentId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, "Student not found"));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully patched a student!`,
+    data: result.student,
+  });
+};
--- src/routers/students.js
+++ src/routers/students.js
@@ -3,6 +3,10 @@ import { Router } from "express";
 import {
   getStudentsController,
   getStudentByIdController,
+  createStudentController,
+  deleteStudentController,
+  upsertStudentController,
+  patchStudentController,
 } from "../controllers/students.js";
 import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 
@@ -11,6 +15,16 @@ const router = Router();
 router.get("/students", ctrlWrapper(getStudentsController));
 
 router.get("/students/:studentId", ctrlWrapper(getStudentByIdController));
+
+//Далі додамо POST-роут /students та використаємо контролер.
+
+router.post("/students", ctrlWrapper(createStudentController));
+
+router.delete("/students/:studentId", ctrlWrapper(deleteStudentController));
+
+router.put("/students/:studentId", ctrlWrapper(upsertStudentController));
+
+router.patch("/students/:studentId", ctrlWrapper(patchStudentController));
 
 export default router;
 
