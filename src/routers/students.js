340dbb42690b688678e1e4ed970567f8f3e649ee
Anna Taranchuk <taranchuk4@yahoo.com>
2024-07-26 19:40:15 +0300

    fix bugs

--- src/routers/students.js
+++ src/routers/students.js
@@ -3,8 +3,8 @@ import { Router } from "express";
 import {
   getStudentsController,
   getStudentByIdController,
-} from "../controllers/students";
-import { ctrlWrapper } from "../utils/ctrlWrapper";
+} from "../controllers/students.js";
+import { ctrlWrapper } from "../utils/ctrlWrapper.js";
 
 const router = Router();
 
